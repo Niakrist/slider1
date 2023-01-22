d09168fe27307edc0916082ef1ca98b05cce64d4
Niakrist <ak_88dron@inbox.ru>
2023-01-22 16:10:15 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,11 +14,15 @@ sliderItems.forEach(function (slide, index) {
     // Добавляем index атрибут
     slide.dataset.index = index;
 
+    // Добавляем data атрибут active для первого / активного слайда
+    sliderItems[0].setAttribute('data-active', '');
+
     // Клик по слайдам
     slide.addEventListener('click', function () {
 
         // скрываем текущий слайд
         this.classList.add('hidden');
+        this.removeAttribute('data-active');
 
         // расчитываем индекс следующего слайда
         let nextSlideIndex = Number(slide.dataset.index) + 1 === sliderItems.length ? 0 : Number(slide.dataset.index) + 1;
@@ -26,5 +30,40 @@ sliderItems.forEach(function (slide, index) {
         // Находим элемен со следующим слайдом и показываем его
         const nextSlide = slider.querySelector(`[data-index='${nextSlideIndex}']`);
         nextSlide.classList.remove('hidden');
+        nextSlide.setAttribute('data-active', '');
     });
-})
+
+});
+
+btnNext.onclick = function () {
+
+    // Скрываем текущий слайд
+    const currentSlide = document.querySelector('[data-active]');
+    const currentSlideIndex = Number(currentSlide.dataset.index);
+    currentSlide.classList.add('hidden');
+    currentSlide.removeAttribute('data-active');
+
+    // Показать следующий слайд
+    let nextSlideIndex = (currentSlideIndex + 1 === sliderItems.length) ? 0 : currentSlideIndex + 1;
+    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`)
+    nextSlide.classList.remove('hidden');
+    nextSlide.setAttribute('data-active', '');
+
+};
+
+btnPrev.addEventListener('click', function () {
+
+    // Скрываем текущий слайд
+    const currentSlide = document.querySelector('[data-active]');
+    const currentSlideIndex = Number(currentSlide.dataset.index);
+    currentSlide.classList.add('hidden');
+    currentSlide.removeAttribute('data-active');
+
+    // Показать следующий слайд
+    let prevSlideIndex = (currentSlideIndex - 1 < 0) ? sliderItems.length - 1 : currentSlideIndex - 1;
+    console.log(prevSlideIndex);
+    const prevSlide = slider.querySelector(`[data-index="${prevSlideIndex}"]`)
+    prevSlide.classList.remove('hidden');
+    prevSlide.setAttribute('data-active', '');
+
+});
